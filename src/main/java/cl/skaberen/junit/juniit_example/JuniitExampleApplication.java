25f53850d4b827f3921e8a9ba762acc99a3ed696
Ismael <icuevas@uft.cl>
2021-04-20 20:46:12 -0400

    firt commit

---
+++ src/main/java/cl/skaberen/junit/juniit_example/JuniitExampleApplication.java
@@ -0,0 +1,13 @@
+package cl.skaberen.junit.juniit_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniitExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniitExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/skaberen/junit/juniit_example/JuniitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.skaberen.junit.juniit_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuniitExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
